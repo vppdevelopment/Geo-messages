a25cc189ffbc259d3b0d06c917e765f7953a7041
edwinvpp <edwin.guerrero.lopez@gmail.com>
2016-03-02 14:01:28 -0500

    Se pintan los geo tags de manera correcta

--- Examples/MapBox/Mapbox _currentLocation/mapbox/currentLocation.js
+++ Examples/MapBox/Mapbox _currentLocation/mapbox/currentLocation.js
@@ -1,5 +1,5 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoidnBwZGV2ZWxvcG1lbnQiLCJhIjoiY2lqb3ZxZndrMDB5anRva280NHNjcmhhOCJ9._KXGbPR4VZjyVE5yuTt5xg';
-var map = L.mapbox.map('map', 'mapbox.streets').setView([4.737229, -74.085872],14);;
+var map = L.mapbox.map('map', 'mapbox.streets').setView([4.65454, -74.05566],14);;
 
 var myLayer = L.mapbox.featureLayer().addTo(map);
 
@@ -11,59 +11,27 @@ if (!navigator.geolocation)
     map.locate();
 }
 
-var messages   = getMessages();
-
-var markers = new L.MarkerClusterGroup();
-
-var title = "Espectacular Rifa Carro 0 Km, ven ya CC Centro Suba";
-var marker = L.marker(new L.LatLng(4.737229, -74.085872), {
-    icon: L.mapbox.marker.icon({'marker-symbol': 'post', 'marker-color': '0044FF'}),
-    title: title
-});
-
-    marker.bindPopup(title);
-    markers.addLayer(marker);
+function init(){
 
 
-    var title = "Happy Hour 3 x 1 en cocteles, Terraza Bar  ";
-    var marker = L.marker(new L.LatLng(4.749108, -74.095383), {
-        icon: L.mapbox.marker.icon({'marker-symbol': 'post', 'marker-color': 'DF0101'}),
-        title: title
+  onMessageLoad(function(snapshot)
+    {
+      var markers = new L.MarkerClusterGroup();
+
+      var title = snapshot.val().message;
+      var marker = L.marker(new L.LatLng(snapshot.val().lat, snapshot.val().long), {
+          icon: L.mapbox.marker.icon({'marker-symbol': 'post', 'marker-color': '0044FF'}),
+          title: title
+      });
+
+          marker.bindPopup(title);
+          markers.addLayer(marker);
+
+          map.addLayer(markers);
     });
 
-        marker.bindPopup(title);
-        markers.addLayer(marker);
 
-map.addLayer(markers);
-
-// Once we've got a position, zoom and center the map
-// on it, and add a single marker.
-map.on('locationfound', function(e) {
-    map.fitBounds(e.bounds);
-
-    myLayer.setGeoJSON({
-        type: 'Feature',
-        geometry: {
-            type: 'Point',
-            coordinates: [e.latlng.lng, e.latlng.lat]
-        },
-        properties: {
-            'title': 'Here I am!',
-            'marker-color': '#ff8888',
-            'marker-symbol': 'star'
-        }
-    });
-
-});
-
-// If the user chooses not to allow their location
-// to be shared, display an error message.
-map.on('locationerror', function() {
-    
-});
-
-
-
+}
 function onmove() {
     // Get the map bounds - the top-left and bottom-right locations.
     var inBounds = [],
