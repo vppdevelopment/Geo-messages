1b617fbd76d5813b735e0563306b4485207acbac
maxneo4 <maxneo4@gmail.com>
2016-03-05 15:42:08 -0500

    Revert "Se pintan mensajes por el momento quemados, pendiente lectura de fireBase"
    This reverts commit 10a4477d43c480a070ee2e1bf4beba1edda98e04.

--- Examples/MapBox/Mapbox _currentLocation/mapbox/currentLocation.js
+++ Examples/MapBox/Mapbox _currentLocation/mapbox/currentLocation.js
@@ -1,8 +1,13 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoidnBwZGV2ZWxvcG1lbnQiLCJhIjoiY2lqb3ZxZndrMDB5anRva280NHNjcmhhOCJ9._KXGbPR4VZjyVE5yuTt5xg';
-var map = L.mapbox.map('map', 'mapbox.streets').setView([4.65454, -74.05566],14);;
+var map = L.mapbox.map('map', 'mapbox.streets');
 
 var myLayer = L.mapbox.featureLayer().addTo(map);
 
+// This uses the HTML5 geolocation API, which is available on
+// most mobile browsers and modern browsers, but not in Internet Explorer
+//
+// See this chart of compatibility for details:
+// http://caniuse.com/#feat=geolocation
 if (!navigator.geolocation)
 {
     alert('Geolocation is not available');
@@ -11,44 +16,29 @@ if (!navigator.geolocation)
     map.locate();
 }
 
-function init(){
 
+// Once we've got a position, zoom and center the map
+// on it, and add a single marker.
+map.on('locationfound', function(e) {
+    map.fitBounds(e.bounds);
 
-  onMessageLoad(function(snapshot)
-    {
-      var markers = new L.MarkerClusterGroup();
-
-      var title = snapshot.val().message;
-      var marker = L.marker(new L.LatLng(snapshot.val().lat, snapshot.val().long), {
-          icon: L.mapbox.marker.icon({'marker-symbol': 'post', 'marker-color': '0044FF'}),
-          title: title
-      });
-
-          marker.bindPopup(title);
-          markers.addLayer(marker);
-
-          map.addLayer(markers);
-    });
-
-
-}
-function onmove() {
-    // Get the map bounds - the top-left and bottom-right locations.
-    var inBounds = [],
-        bounds = map.getBounds();
-    markers.eachLayer(function(marker) {
-        // For each marker, consider whether it is currently visible by comparing
-        // with the current map bounds.
-        if (bounds.contains(marker.getLatLng())) {
-            inBounds.push(marker.options.title);
+    myLayer.setGeoJSON({
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [e.latlng.lng, e.latlng.lat]
+        },
+        properties: {
+            'title': 'Here I am!',
+            'marker-color': '#ff8888',
+            'marker-symbol': 'star'
         }
     });
-    // Display a list of markers.
-    document.getElementById('coordinates').innerHTML = inBounds.join('\n');
-}
 
-map.on('move', onmove);
+});
 
-// call onmove off the bat so that the list is populated.
-// otherwise, there will be no markers listed until the map is moved.
-onmove();
+// If the user chooses not to allow their location
+// to be shared, display an error message.
+map.on('locationerror', function() {
+    alert('Position could not be found');
+});
