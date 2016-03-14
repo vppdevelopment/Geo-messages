88a6820c59b8d13d78e07cf2900fd7ff70e922ae
sebas8824 <sebastian.benitez@outlook.com>
2016-03-14 10:42:49 -0500

    Delete funcionality added

--- vpp-app/app/home/home.js
+++ vpp-app/app/home/home.js
@@ -1,24 +1,28 @@
 'use strict';
- 
-angular.module('myApp.home', ['ngRoute','firebase']) 
-// Declared route 
+
+angular.module('myApp.home', ['ngRoute','firebase'])
+// Declared route
 .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/home', {
         templateUrl: 'home/home.html',
         controller: 'HomeCtrl'
     });
 }])
- 
+
 // Home controller
 .controller('HomeCtrl', ['$scope','$location','CommonProp','$firebaseAuth',function($scope,$location,CommonProp,$firebaseAuth) {
 	var firebaseObj = new Firebase("https://vpp-views.firebaseio.com");
  	var loginObj = $firebaseAuth(firebaseObj);
+  var login = {};
+
+  $scope.login = login;
 
  	$scope.SignIn = function(event) {
     event.preventDefault();  // To prevent form refresh
+    login.loading = true;
     var username = $scope.user.email;
     var password = $scope.user.password;
-     
+
     loginObj.$authWithPassword({
             email: username,
             password: password
@@ -26,10 +30,12 @@ angular.module('myApp.home', ['ngRoute','firebase'])
         .then(function(user) {
             // Success callback
             CommonProp.setUser(user.password.email);
+            login.loading = true;
 			$location.path('/welcome');
         }, function(error) {
             // Failure callback
             console.log('Authentication failure');
+            login.loading = false;
         });
 	}
 }])
@@ -43,4 +49,23 @@ angular.module('myApp.home', ['ngRoute','firebase'])
             user = value;
         }
     };
-});
+})
+.directive('laddaLoading', [
+    function() {
+        return {
+            link: function(scope, element, attrs) {
+                var Ladda = window.Ladda;
+                var ladda = Ladda.create(element[0]);
+                // Watching login.loading for change
+                scope.$watch(attrs.laddaLoading, function(newVal, oldVal) {
+                    // Based on the value start and stop the indicator
+                    if (newVal) {
+                        ladda.start();
+                    } else {
+                        ladda.stop();
+                    }
+                });
+            }
+        };
+    }
+]);
